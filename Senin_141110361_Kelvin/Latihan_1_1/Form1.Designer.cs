577b1e9db9cb791f4fae33f7148655e0ddae3fb9
KelvinCosma <kelvincosma00@gmail.com>
2016-09-30 21:48:27 +0700

    First Commit

---
+++ Senin_141110361_Kelvin/Latihan_1_1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Latihan_1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.MinTxt = new System.Windows.Forms.Label();
+            this.MaxTxt = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.DateStat = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Scrollvalue2 = new System.Windows.Forms.Label();
+            this.Scrollvalue1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Calendar.png");
+            this.imageList1.Images.SetKeyName(1, "Cog_B.png");
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.ImageList = this.imageList1;
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(280, 338);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.Scrollvalue2);
+            this.tabPage1.Controls.Add(this.Scrollvalue1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.DateStat);
+            this.tabPage1.Controls.Add(this.hScrollBar2);
+            this.tabPage1.Controls.Add(this.hScrollBar1);
+            this.tabPage1.ImageKey = "Calendar.png";
+            this.tabPage1.Location = new System.Drawing.Point(4, 23);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(272, 311);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Calendar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.ImageKey = "Cog_B.png";
+            this.tabPage2.Location = new System.Drawing.Point(4, 23);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(272, 311);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Other";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // MinTxt
+            // 
+            this.MinTxt.AutoSize = true;
+            this.MinTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinTxt.Location = new System.Drawing.Point(12, 356);
+            this.MinTxt.Name = "MinTxt";
+            this.MinTxt.Size = new System.Drawing.Size(87, 17);
+            this.MinTxt.TabIndex = 1;
+            this.MinTxt.Text = "Min Value : -";
+            // 
+            // MaxTxt
+            // 
+            this.MaxTxt.AutoSize = true;
+            this.MaxTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaxTxt.Location = new System.Drawing.Point(156, 356);
+            this.MaxTxt.Name = "MaxTxt";
+            this.MaxTxt.Size = new System.Drawing.Size(90, 17);
+            this.MaxTxt.TabIndex = 2;
+            this.MaxTxt.Text = "Max Value : -";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 1;
+            this.hScrollBar1.Location = new System.Drawing.Point(20, 60);
+            this.hScrollBar1.Maximum = 10;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(200, 20);
+            this.hScrollBar1.TabIndex = 0;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.TabPage1_Scrolled);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.LargeChange = 1;
+            this.hScrollBar2.Location = new System.Drawing.Point(20, 86);
+            this.hScrollBar2.Maximum = 10;
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(200, 20);
+            this.hScrollBar2.TabIndex = 1;
+            this.hScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.TabPage1_Scrolled);
+            // 
+            // DateStat
+            // 
+            this.DateStat.CustomFormat = "dd/MM/yyyy";
+            this.DateStat.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DateStat.Location = new System.Drawing.Point(20, 120);
+            this.DateStat.Name = "DateStat";
+            this.DateStat.Size = new System.Drawing.Size(200, 20);
+            this.DateStat.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(188, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "10";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "5";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Scrollvalue2
+            // 
+            this.Scrollvalue2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Scrollvalue2.Location = new System.Drawing.Point(225, 86);
+            this.Scrollvalue2.Name = "Scrollvalue2";
+            this.Scrollvalue2.Size = new System.Drawing.Size(41, 20);
+            this.Scrollvalue2.TabIndex = 8;
+            this.Scrollvalue2.Text = "0";
+            this.Scrollvalue2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Scrollvalue1
+            // 
+            this.Scrollvalue1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Scrollvalue1.Location = new System.Drawing.Point(225, 60);
+            this.Scrollvalue1.Name = "Scrollvalue1";
+            this.Scrollvalue1.Size = new System.Drawing.Size(41, 20);
+            this.Scrollvalue1.TabIndex = 7;
+            this.Scrollvalue1.Text = "0";
+            this.Scrollvalue1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 382);
+            this.Controls.Add(this.MaxTxt);
+            this.Controls.Add(this.MinTxt);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker DateStat;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label MinTxt;
+        private System.Windows.Forms.Label MaxTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Scrollvalue2;
+        private System.Windows.Forms.Label Scrollvalue1;
+    }
+}
+
---
+++ Senin_141110361_Kelvin/Latihan_1_1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TabPage1_Scrolled(object sender, ScrollEventArgs e)
+        {
+            int A = hScrollBar1.Value > hScrollBar2.Value ? hScrollBar2.Value : hScrollBar1.Value;
+            int B = hScrollBar1.Value > hScrollBar2.Value ? hScrollBar1.Value : hScrollBar2.Value;
+            Scrollvalue1.Text = hScrollBar1.Value.ToString();
+            Scrollvalue2.Text = hScrollBar2.Value.ToString();
+            MinTxt.Text = "Min Value : " + A.ToString();
+            MaxTxt.Text = "Max Value : " + B.ToString();
+            DateStat.MinDate = DateTime.Today.AddYears(-A);
+            DateStat.MaxDate = DateTime.Today.AddYears(B);
+        }
+    }
+}
