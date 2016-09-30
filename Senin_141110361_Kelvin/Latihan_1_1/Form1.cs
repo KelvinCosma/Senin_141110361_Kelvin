30e29e5e42b55aa481a705a2e51b9df69bdbb0d8
KelvinCosma <kelvincosma00@gmail.com>
2016-09-30 21:57:45 +0700

    First Commit

--- Senin_141110361_Kelvin/Latihan_1_1/Form1.cs
+++ Senin_141110361_Kelvin/Latihan_1_1/Form1.cs
@@ -25,8 +25,8 @@ namespace Latihan_1_1
             Scrollvalue2.Text = hScrollBar2.Value.ToString();
             MinTxt.Text = "Min Value : " + A.ToString();
             MaxTxt.Text = "Max Value : " + B.ToString();
-            DateStat.MinDate = DateTime.Today.AddYears(-A);
-            DateStat.MaxDate = DateTime.Today.AddYears(B);
+            DateStat.MinDate = DateTime.Today;
+            DateStat.MaxDate = DateTime.Today.AddYears(Math.Abs(A - B));
         }
     }
 }
