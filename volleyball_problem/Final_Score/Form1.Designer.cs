67f607af9a7dbff29319bff7e2c5128de59ee29d
KelvinCosma <kelvincosma00@gmail.com>
2016-11-25 22:11:47 +0700

    volleyball problem solution

---
+++ volleyball_problem/Final_Score/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Final_Score
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt1 = new System.Windows.Forms.TextBox();
+            this.Txt2 = new System.Windows.Forms.TextBox();
+            this.TxtHasil = new System.Windows.Forms.TextBox();
+            this.BtnHitung = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Txt1
+            // 
+            this.Txt1.Location = new System.Drawing.Point(128, 12);
+            this.Txt1.Name = "Txt1";
+            this.Txt1.Size = new System.Drawing.Size(57, 20);
+            this.Txt1.TabIndex = 0;
+            // 
+            // Txt2
+            // 
+            this.Txt2.Location = new System.Drawing.Point(128, 38);
+            this.Txt2.Name = "Txt2";
+            this.Txt2.Size = new System.Drawing.Size(57, 20);
+            this.Txt2.TabIndex = 1;
+            // 
+            // TxtHasil
+            // 
+            this.TxtHasil.Location = new System.Drawing.Point(128, 63);
+            this.TxtHasil.Name = "TxtHasil";
+            this.TxtHasil.Size = new System.Drawing.Size(164, 20);
+            this.TxtHasil.TabIndex = 0;
+            // 
+            // BtnHitung
+            // 
+            this.BtnHitung.Location = new System.Drawing.Point(191, 12);
+            this.BtnHitung.Name = "BtnHitung";
+            this.BtnHitung.Size = new System.Drawing.Size(101, 46);
+            this.BtnHitung.TabIndex = 2;
+            this.BtnHitung.Text = "Press";
+            this.BtnHitung.UseVisualStyleBackColor = true;
+            this.BtnHitung.Click += new System.EventHandler(this.BtnHitung_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Team A\'s Final Score";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Team B\'s Final Score";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Possible Max Round";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 94);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnHitung);
+            this.Controls.Add(this.TxtHasil);
+            this.Controls.Add(this.Txt2);
+            this.Controls.Add(this.Txt1);
+            this.MaximumSize = new System.Drawing.Size(320, 132);
+            this.MinimumSize = new System.Drawing.Size(320, 132);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Final Score";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt1;
+        private System.Windows.Forms.TextBox Txt2;
+        private System.Windows.Forms.TextBox TxtHasil;
+        private System.Windows.Forms.Button BtnHitung;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ volleyball_problem/Final_Score/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Score
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TxtHasil.Enabled = false;
+        }
+
+        public const int Modular = 1000000007;
+
+        public void Swap(ref int A, ref int B) { int temp = B; B = A; A = temp; }
+
+        public long Prob(int A, int B) {
+            if (!((A >= 25 && B == A - 2) || (A == 25 && B < 24))) { return 0; }
+            else {
+                if (A > 26) {
+                    long result = nCr(48, 24);
+                    A -= 24; B -= 24;
+                    result = result ^ nCr(A + B - 1, B);
+                    return result;
+                }
+                else {
+                    A += B; A -= 1;
+                    return nCr(A, B);
+                }
+            }
+        }
+
+
+        public long[] FactorialDivision(int numerator, int denominator) {
+            long[] numer = new long[numerator];
+            long[] denomin = new long[denominator];
+            for (int i = 0; i < numerator; i++) { numer[i] = numerator - i; }
+            for (int j = 0; j < denominator; j++) { denomin[j] = denominator - j; }
+            for (int i = 0; i < numer.Length; i++) {
+                for (int j = 0; j < denomin.Length; j++) {
+                    if ((numer[i] != 1 && denomin[j] != 1) && (numer[i] == denomin[j])) {
+                        numer[i] = 1;
+                        denomin[j] = 1;
+                        break;
+                    }
+                }
+            }
+            return numer;
+        }
+
+        public long nCr(int n, int r) {
+            long result = 1;
+            long[] numer = FactorialDivision(n, n - r);
+            long[] denomin = new long[r];
+            for (int j = 0; j < r; j++) { denomin[j] = r - j; }
+            for (int i = 0; i < numer.Length; i++) {
+                for (int j = 0; j < denomin.Length; j++) {
+                    if (denomin[j] != 1 && (numer[i] % denomin[j] == 0)) {
+                        numer[i] /= denomin[j]; denomin[j] = 1; break;
+                    }
+                }
+            }
+            for (int i = 0; i < numer.Length; i++) { result *= numer[i]; }
+            for (int i = 0; i < denomin.Length; i++) { result /= denomin[i]; }
+            return result;
+        }
+
+        private void BtnHitung_Click(object sender, EventArgs e) {
+            int A, B;
+            A = int.Parse(Txt1.Text);
+            B = int.Parse(Txt2.Text);
+            if (A < B) { Swap(ref A, ref B); }
+            long probability = Prob(A, B);
+            if (probability > Modular) { probability %= Modular; }
+            TxtHasil.Text = probability + "";
+        }
+    }
+}
