683e9e8ecca6be51a082daadcf7ace96a3ebd1f0
KelvinCosma <kelvincosma00@gmail.com>
2016-12-31 00:00:55 +0700

    Revisi Latihan_pos

--- Senin_141110361_Kelvin/Latihan_pos/Data Form/DataList.cs
+++ Senin_141110361_Kelvin/Latihan_pos/Data Form/DataList.cs
@@ -14,6 +14,10 @@ namespace Latihan_pos
 {
     public partial class DataList : MetroFramework.Forms.MetroForm
     {
+        private DataTable dtBarang;
+        private DataTable dtCustomer;
+        private DataTable dtSupplier;
+
         public DataList()
         {
             InitializeComponent();
@@ -46,11 +50,13 @@ namespace Latihan_pos
             TextFilterBarang.ForeColor = Color.FromArgb(32, 160, 64);
             TextFilterBarang.UseCustomBackColor = TextFilterBarang.UseCustomForeColor = true;
             TextFilterBarang.TextChanged += new EventHandler(TextFilterBarang_TextChanged);
+            TextFilterBarang.Tag = "Filter Barang";
 
             DataGridView dgvDatabaseBarang = new DataGridView();
             dgvDatabaseBarang.Font = new Font("Segoe UI", 8F, FontStyle.Bold);
             dgvDatabaseBarang.Location = new Point(32, 64);
             dgvDatabaseBarang.Size = new Size(DataListTabControl.Size.Width - 64, 432);
+            dgvDatabaseBarang.Tag = "Database Barang";
 
             DatabaseBarangPage.Controls.Add(FilterBarang);
             DatabaseBarangPage.Controls.Add(TextFilterBarang);
@@ -69,11 +75,13 @@ namespace Latihan_pos
             TextFilterCustomer.ForeColor = Color.FromArgb(32, 160, 64);
             TextFilterCustomer.UseCustomBackColor = TextFilterCustomer.UseCustomForeColor = true;
             TextFilterCustomer.TextChanged += new EventHandler(TextFilterCustomer_TextChanged);
+            TextFilterCustomer.Tag = "Filter Customer";
 
             DataGridView dgvDatabaseCustomer = new DataGridView();
             dgvDatabaseCustomer.Font = new Font("Segoe UI", 8F, FontStyle.Bold);
             dgvDatabaseCustomer.Location = new Point(32, 64);
             dgvDatabaseCustomer.Size = new Size(DataListTabControl.Size.Width - 64, 432);
+            dgvDatabaseCustomer.Tag = "Database Customer";
 
             DatabaseCustomerPage.Controls.Add(FilterCustomer);
             DatabaseCustomerPage.Controls.Add(TextFilterCustomer);
@@ -92,30 +100,115 @@ namespace Latihan_pos
             TextFilterSupplier.ForeColor = Color.FromArgb(32, 160, 64);
             TextFilterSupplier.UseCustomBackColor = TextFilterSupplier.UseCustomForeColor = true;
             TextFilterSupplier.TextChanged += new EventHandler(TextFilterSupplier_TextChanged);
+            TextFilterSupplier.Tag = "Filter Supplier";
 
             DataGridView dgvDatabaseSupplier = new DataGridView();
             dgvDatabaseSupplier.Font = new Font("Segoe UI", 8F, FontStyle.Bold);
             dgvDatabaseSupplier.Location = new Point(32, 64);
             dgvDatabaseSupplier.Size = new Size(DataListTabControl.Size.Width - 64, 432);
+            dgvDatabaseSupplier.Tag = "Database Supplier";
 
             DatabaseSupplierPage.Controls.Add(FilterSupplier);
             DatabaseSupplierPage.Controls.Add(TextFilterSupplier);
             DatabaseSupplierPage.Controls.Add(dgvDatabaseSupplier);
+
+            dgvDatabaseBarang.DataSource = Class.Barang.SelectAll();
+            dgvDatabaseBarang.Refresh();
+            dgvDatabaseCustomer.DataSource = Class.Customer.SelectAll();
+            dgvDatabaseCustomer.Refresh();
+            dgvDatabaseSupplier.DataSource = Class.Supplier.SelectAll();
+            dgvDatabaseSupplier.Refresh();
+
+            dtBarang = (DataTable)dgvDatabaseBarang.DataSource;
+            dtCustomer = (DataTable)dgvDatabaseCustomer.DataSource;
+            dtSupplier = (DataTable)dgvDatabaseSupplier.DataSource;
         }
 
         private void TextFilterBarang_TextChanged(object sender, EventArgs e)
         {
-
+            int i = 0;
+            string filter = "", textFilterBarang = "";
+            BindingSource bs = new BindingSource();
+            foreach (Control item in DatabaseBarangPage.Controls) {
+                if (item is DataGridView) {
+                    if (item.Tag.ToString() == "Database Barang") { bs.DataSource = (item as DataGridView).DataSource; }
+                }
+            }
+            foreach (Control item in DatabaseBarangPage.Controls) {
+                if (item is DataGridView) {
+                    if (item.Tag.ToString() == "Filter Barang") { textFilterBarang = item.Text; }
+                }
+            }
+            foreach (DataColumn dc in dtBarang.Columns) {
+                if (i > 0) { filter += " or "; }
+                filter += dc.ColumnName + " like '%" + textFilterBarang + "%'";
+                i++;
+            }
+            bs.Filter = filter;
+            Console.WriteLine(bs.Filter);
+            foreach (Control item in DatabaseBarangPage.Controls) {
+                if (item is DataGridView) {
+                    if (item.Tag.ToString() == "Database Barang") { bs.DataSource = (item as DataGridView).DataSource; }
+                }
+            }
         }
 
         private void TextFilterCustomer_TextChanged(object sender, EventArgs e)
         {
-
+            int i = 0;
+            string filter = "", textFilterCustomer = "";
+            BindingSource bs = new BindingSource();
+            foreach (Control item in DatabaseCustomerPage.Controls) {
+                if (item is DataGridView) {
+                    if (item.Tag.ToString() == "Database Customer") { bs.DataSource = (item as DataGridView).DataSource; }
+                }
+            }
+            foreach (Control item in DatabaseCustomerPage.Controls) {
+                if (item is DataGridView) {
+                    if (item.Tag.ToString() == "Filter Customer") { textFilterCustomer = item.Text; }
+                }
+            }
+            foreach (DataColumn dc in dtCustomer.Columns) {
+                if (i > 0) { filter += " or "; }
+                filter += dc.ColumnName + " like '%" + textFilterCustomer + "%'";
+                i++;
+            }
+            bs.Filter = filter;
+            Console.WriteLine(bs.Filter);
+            foreach (Control item in DatabaseCustomerPage.Controls) {
+                if (item is DataGridView) {
+                    if (item.Tag.ToString() == "Database Customer") { bs.DataSource = (item as DataGridView).DataSource; }
+                }
+            }
         }
 
         private void TextFilterSupplier_TextChanged(object sender, EventArgs e)
         {
-
+            int i = 0;
+            string filter = "", textFilterSupplier = "";
+            BindingSource bs = new BindingSource();
+            foreach (Control item in DatabaseSupplierPage.Controls) {
+                if (item is DataGridView) {
+                    if (item.Tag.ToString() == "Database Supplier") { bs.DataSource = (item as DataGridView).DataSource; }
+                }
+            }
+            foreach (Control item in DatabaseSupplierPage.Controls) {
+                if (item is DataGridView) {
+                    if (item.Tag.ToString() == "Filter Supplier") { textFilterSupplier = item.Text; }
+                }
+            }
+            foreach (DataColumn dc in dtSupplier.Columns) {
+                if (i > 0) { filter += " or "; }
+                filter += dc.ColumnName + " like '%" + textFilterSupplier + "%'";
+                i++;
+            }
+            bs.Filter = filter;
+            Console.WriteLine(bs.Filter);
+            foreach (Control item in DatabaseSupplierPage.Controls) {
+                if (item is DataGridView) {
+                    if (item.Tag.ToString() == "Database Supplier") { bs.DataSource = (item as DataGridView).DataSource; }
+                }
+            }
         }
 
         private void Back_Click(object sender, EventArgs e)
